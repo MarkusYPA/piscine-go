b1bb1a92e08cb5a14af4521b73d3eced583cdbff
Markus Amberla <markus.amberla@gmail.com>
2024-05-06 18:49:35 +0300

    Hero's fam

--- myfamily.sh
+++ myfamily.sh
@@ -2,4 +2,4 @@
 
 id=$HERO_ID
 
-curl -s https://01.gritlab.ax/assets/superhero/all.json | jq -r ' .[] | select( .id == '$id') | .connections | .relatives'
+curl -s https://01.gritlab.ax/assets/superhero/all.json | jq ' .[] | select( .id == '$id') | .connections | .relatives' -r
