3fd5e8dd6e8d97992f5572508ba4620d77d89cda
Markus Amberla <markus.amberla@gmail.com>
2024-05-08 11:38:05 +0300

    prolly wrong

---
+++ printnbr.go
@@ -0,0 +1,14 @@
+package piscine
+
+import (
+	"fmt"
+	//"github.com/01-edu/z01"
+)
+
+func PrintNbr(n int) {
+	//bn := 48 // base number for printing numbers
+
+	//z01.PrintRune(rune(n + bn))
+
+	fmt.Print(n)
+}
