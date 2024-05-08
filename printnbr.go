29f58bbae71c297b687ed9f1f8e04f81b564b290
Markus Amberla <markus.amberla@gmail.com>
2024-05-08 13:04:10 +0300

    numbas

--- printnbr.go
+++ printnbr.go
@@ -1,5 +1,30 @@
 package piscine
 
+import (
+	"github.com/01-edu/z01"
+)
+
 func PrintNbr(n int) {
-	print(n)
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
+	if n < 0 {
+		z01.PrintRune('-')
+		n = n * -1
+	}
+
+	runes := []rune{}
+	tempN := n
+	bn := 48
+
+	for tempN > 0 {
+		runes = append(runes, rune((tempN%10)+bn))
+		tempN = tempN / 10
+	}
+
+	for i := len(runes) - 1; i > -1; i-- {
+		z01.PrintRune(runes[i])
+	}
 }
