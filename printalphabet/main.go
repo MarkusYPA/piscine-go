215867b17210bec553bedfc6b8026da9747b251b
Markus Amberla <markus.amberla@gmail.com>
2024-05-08 08:51:30 +0300

    lower case letters

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
@@ -9,4 +11,5 @@ func main() {
 	for i := 0; i < 26; i++ {
 		fmt.Printf("%c", 97+i)
 	}
+	z01.PrintRune('\n')
 }
