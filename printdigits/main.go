62246d6bea51973ebc702a586e9949f14ca2108c
Markus Amberla <markus.amberla@gmail.com>
2024-05-08 09:13:04 +0300

    numbers

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	for i := 0; i < 10; i++ {
-		fmt.Print(i)
+		z01.PrintRune(rune(i + 48))
 	}
 	z01.PrintRune('\n')
 }
