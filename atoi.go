32bb1dbdbac87a3a9ded30a9093c8c63fce09e12
Christopher Fremond <dev@01-edu.org>
2024-05-09 08:53:26 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -3,35 +3,32 @@ package piscine
 func Atoi(s string) int {
 	factor := 1
 
-	if len(s) > 0 {
-		if s[0] == 43 {
-			s = s[1:]
-		}
+	if s[0] == '+' || s[0] == '-' {
 
-		if s[0] == 45 {
+		if s[0] == '-' {
 			factor = -1
-			s = s[1:]
 		}
 
-		ints := make([]int, len(s))
-
-		for i, v := range s {
-			if v < 48 || v > 57 {
-				return 0
-			}
-			ints[i] = int(v - 48)
-		}
-
-		result := 0
-		mult := 1
-
-		for i := len(s) - 1; i > -1; i-- {
-			result += ints[i] * mult
-			mult *= 10
-		}
-
-		return result * factor
-	} else {
-		return 0
+		s = s[1:]
 	}
+
+	ints := make([]int, len(s))
+
+	for i, v := range s {
+		if v < 48 || v > 57 {
+			return 0
+		}
+		ints[i] = int(v - 48)
+	}
+
+	result := 0
+	mult := 1
+
+	for i := len(s) - 1; i > -1; i-- {
+		result += ints[i] * mult
+		mult *= 10
+	}
+
+	return result * factor
+
 }
