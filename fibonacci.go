400659af8b2e23310360358f8e62184435eab351
Markus Amberla <markus.amberla@gmail.com>
2024-05-13 10:47:38 +0300

    Fibo

---
+++ fibonacci.go
@@ -0,0 +1,19 @@
+package piscine
+
+// Store values on this map
+var fibMemo = make(map[int]int)
+
+func Fibonacci(index int) int {
+	// Return from list if already calculated. map[i] returns value AND bool
+	if val, ok := fibMemo[index]; ok {
+		return val
+	}
+
+	if index < 2 {
+		fibMemo[index] = index // First store, then return
+		return index
+	}
+
+	fibMemo[index] = Fibonacci(index-1) + Fibonacci(index-2) // First store, then return
+	return fibMemo[index]
+}
