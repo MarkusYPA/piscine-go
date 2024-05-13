096ad1bfde8063aa6d60d0867d1ae430683c5d4b
Markus Amberla <markus.amberla@gmail.com>
2024-05-13 11:15:55 +0300

    Sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,13 +1,33 @@
 package piscine
 
-import "math"
-
 func Sqrt(nb int) int {
-	result := math.Sqrt(float64(nb))
-
-	if result-float64(int(result)) != 0 {
+	if nb < 0 {
 		return 0
-	} else {
-		return int(result)
 	}
+
+	if nb == 0 || nb == 1 {
+		return nb
+	}
+
+	result := powers(nb)
+
+	if result*result == nb {
+		return result
+	} else {
+		return 0
+	}
+}
+
+func powers(nb int) int {
+	powa := 1
+
+	for i := 1; i < nb; i++ {
+		powa = i * i
+
+		if powa >= nb {
+			return i
+		}
+	}
+
+	return 0
 }
