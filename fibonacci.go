df83cb6c48c3a968db48c0b4ceb9afa33c3c9aa3
Markus Amberla <markus.amberla@gmail.com>
2024-05-13 10:48:58 +0300

    Fibo

--- fibonacci.go
+++ fibonacci.go
@@ -4,6 +4,10 @@ package piscine
 var fibMemo = make(map[int]int)
 
 func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+
 	// Return from list if already calculated. map[i] returns value AND bool
 	if val, ok := fibMemo[index]; ok {
 		return val
