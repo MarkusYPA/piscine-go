f52a5fb72cbe4acd5f085b0b0741a641e680022b
Markus Amberla <markus.amberla@gmail.com>
2024-05-14 12:10:59 +0300

    Join

---
+++ join.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	result := ""
+	for i, v := range strs {
+		result += v
+		if i < len(strs)-1 {
+			result += sep
+		}
+	}
+
+	return result
+}
