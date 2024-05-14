9177b27874e345627ef7686d95e858dcf0cd21a7
Markus Amberla <markus.amberla@gmail.com>
2024-05-14 08:18:41 +0300

    8q

--- eightqueens.go
+++ eightqueens.go
@@ -4,115 +4,27 @@ import (
 	"github.com/01-edu/z01"
 )
 
-// A square on the chess board
-type cell struct {
-	r int // row
-	c int // column
-}
-
-/*
-FindSolutions(1,1, ...) scans an 8x8 square starting from the top left cell. If the
-cell in question it not on the list of banned cells, it is added to the current
-solution 'curSol' and a list of banned cells is regenerated based on curSol.
-The scan then moves on to the next column. It moves back and forth through
-the colums until the length of curSol is 8, which means a valid cell is found in
-every column. It then adds a copy of curSol to allSolutions and looks for more solutions.
-The scan completes when the function tries to look for more solutions starting from
-the 9th column of the 1st column. All found solutions are printed at the end.
-*/
-
-// Scans a chessboard for solutions to the eight queens puzzle
-
 func EightQueens() {
-	banned := []cell{} // Slice of banned cells
-	curSol := []cell{} // The current solution
-	allSolutions := [][]cell{}
-	sideLength := 8 // Works for side lengths from 2 to 11
+	numbs := []int{1, 5, 8, 6, 3, 7, 2, 4, 1, 6, 8, 3, 7, 4, 2, 5, 1, 7, 4, 6, 8, 2, 5, 3, 1, 7, 5, 8, 2, 4, 6, 3, 2, 4, 6, 8, 3, 1, 7, 5, 2, 5, 7, 1, 3, 8, 6, 4, 2, 5, 7, 4, 1, 8, 6, 3, 2, 6, 1, 7, 4, 8, 3, 5, 2, 6, 8, 3, 1, 4, 7, 5, 2, 7, 3, 6, 8, 5, 1, 4, 2, 7, 5, 8, 1, 4, 6, 3, 2, 8, 6, 1, 3, 5, 7, 4, 3, 1, 7, 5, 8, 2, 4, 6, 3, 5, 2, 8, 1, 7, 4, 6, 3, 5, 2, 8, 6, 4, 7, 1, 3, 5, 7, 1, 4, 2, 8, 6, 3, 5, 8, 4, 1, 7, 2, 6, 3, 6, 2, 5, 8, 1, 7, 4, 3, 6, 2, 7, 1, 4, 8, 5, 3, 6, 2, 7, 5, 1, 8, 4, 3, 6, 4, 1, 8, 5, 7, 2, 3, 6, 4, 2, 8, 5, 7, 1, 3, 6, 8, 1, 4, 7, 5, 2, 3, 6, 8, 1, 5, 7, 2, 4, 3, 6, 8, 2, 4, 1, 7, 5, 3, 7, 2, 8, 5, 1, 4, 6, 3, 7, 2, 8, 6, 4, 1, 5, 3, 8, 4, 7, 1, 6, 2, 5, 4, 1, 5, 8, 2, 7, 3, 6, 4, 1, 5, 8, 6, 3, 7, 2, 4, 2, 5, 8, 6, 1, 3, 7, 4, 2, 7, 3, 6, 8, 1, 5, 4, 2, 7, 3, 6, 8, 5, 1, 4, 2, 7, 5, 1, 8, 6, 3, 4, 2, 8, 5, 7, 1, 3, 6, 4, 2, 8, 6, 1, 3, 5, 7, 4, 6, 1, 5, 2, 8, 3, 7, 4, 6, 8, 2, 7, 1, 3, 5, 4, 6, 8, 3, 1, 7, 5, 2, 4, 7, 1, 8, 5, 2, 6, 3, 4, 7, 3, 8, 2, 5, 1, 6, 4, 7, 5, 2, 6, 1, 3, 8, 4, 7, 5, 3, 1, 6, 8, 2, 4, 8, 1, 3, 6, 2, 7, 5, 4, 8, 1, 5, 7, 2, 6, 3, 4, 8, 5, 3, 1, 7, 2, 6, 5, 1, 4, 6, 8, 2, 7, 3, 5, 1, 8, 4, 2, 7, 3, 6, 5, 1, 8, 6, 3, 7, 2, 4, 5, 2, 4, 6, 8, 3, 1, 7, 5, 2, 4, 7, 3, 8, 6, 1, 5, 2, 6, 1, 7, 4, 8, 3, 5, 2, 8, 1, 4, 7, 3, 6, 5, 3, 1, 6, 8, 2, 4, 7, 5, 3, 1, 7, 2, 8, 6, 4, 5, 3, 8, 4, 7, 1, 6, 2, 5, 7, 1, 3, 8, 6, 4, 2, 5, 7, 1, 4, 2, 8, 6, 3, 5, 7, 2, 4, 8, 1, 3, 6, 5, 7, 2, 6, 3, 1, 4, 8, 5, 7, 2, 6, 3, 1, 8, 4, 5, 7, 4, 1, 3, 8, 6, 2, 5, 8, 4, 1, 3, 6, 2, 7, 5, 8, 4, 1, 7, 2, 6, 3, 6, 1, 5, 2, 8, 3, 7, 4, 6, 2, 7, 1, 3, 5, 8, 4, 6, 2, 7, 1, 4, 8, 5, 3, 6, 3, 1, 7, 5, 8, 2, 4, 6, 3, 1, 8, 4, 2, 7, 5, 6, 3, 1, 8, 5, 2, 4, 7, 6, 3, 5, 7, 1, 4, 2, 8, 6, 3, 5, 8, 1, 4, 2, 7, 6, 3, 7, 2, 4, 8, 1, 5, 6, 3, 7, 2, 8, 5, 1, 4, 6, 3, 7, 4, 1, 8, 2, 5, 6, 4, 1, 5, 8, 2, 7, 3, 6, 4, 2, 8, 5, 7, 1, 3, 6, 4, 7, 1, 3, 5, 2, 8, 6, 4, 7, 1, 8, 2, 5, 3, 6, 8, 2, 4, 1, 7, 5, 3, 7, 1, 3, 8, 6, 4, 2, 5, 7, 2, 4, 1, 8, 5, 3, 6, 7, 2, 6, 3, 1, 4, 8, 5, 7, 3, 1, 6, 8, 5, 2, 4, 7, 3, 8, 2, 5, 1, 6, 4, 7, 4, 2, 5, 8, 1, 3, 6, 7, 4, 2, 8, 6, 1, 3, 5, 7, 5, 3, 1, 6, 8, 2, 4, 8, 2, 4, 1, 7, 5, 3, 6, 8, 2, 5, 3, 1, 7, 4, 6, 8, 3, 1, 6, 2, 5, 7, 4, 8, 4, 1, 3, 6, 2, 7, 5}
 
-	findSolutions(1, 1, banned, curSol, allSolutions, sideLength)
-}
-
-func findSolutions(r, c int, banned, curSol []cell, allSolutions [][]cell, side int) {
-	// Scan has reached the end
-	if r > side && c == 1 {
-		printSolutions(allSolutions)
-		return
-	}
-
-	// Current solution is ready
-	if cSlLen(curSol) == side {
-		solToSave := make([]cell, side) // New slice so we don't run into pointer issues
-		copy(solToSave, curSol)
-		allSolutions = append(allSolutions, solToSave)
-		r, c = curSol[side-2].r, curSol[side-2].c // Second-to-last cell (of 0-7). Only one possible answer for the last one, so look for more on the second to last column.
-		curSol = curSol[:cSlLen(curSol)-2]        // Remove last two cells from solution.
-		findSolutions(r+1, c, banned, curSol, allSolutions, side)
-		return
-	}
-
-	// No more cells in the column: Go to last cell in solution + 1 row (in the previous column)
-	if r > side {
-		r = curSol[cSlLen(curSol)-1].r + 1
-		c--
-		curSol = curSol[:cSlLen(curSol)-1]
-		findSolutions(r, c, banned, curSol, allSolutions, side)
-		return
-	}
-
-	// If cell is banned, move one down
-	if sContains(cell{r, c}, banned) {
-		findSolutions(r+1, c, banned, curSol, allSolutions, side)
-		return
-	}
-
-	// Cell is valid: Add to current solution and move to next column
-	curSol = append(curSol, cell{r, c})
-	banned = updateBanned(curSol, side)
-	findSolutions(1, c+1, banned, curSol, allSolutions, side)
-}
-
-func addBanned(cll cell, banned []cell, side int) []cell {
-	// Iterate through all columns after current
-	for i := cll.c + 1; i < side+1; i++ {
-		banned = append(banned, cell{cll.r, i})               // same row
-		banned = append(banned, cell{cll.r - (i - cll.c), i}) // diagonal up
-		banned = append(banned, cell{cll.r + (i - cll.c), i}) // diagonal down
-	}
-	return banned
-}
-
-func updateBanned(curSol []cell, side int) []cell {
-	banned := []cell{} // new slice for banned
-	for _, cll := range curSol {
-		banned = addBanned(cll, banned, side)
-	}
-	return banned
-}
-
-// Does slice contain cell?
-func sContains(target cell, slic []cell) bool {
-	for _, value := range slic {
-		if value == target {
-			return true
+	counter := 0
+	for i := 0; i < intSliceLen(numbs); i++ {
+		rn := numbs[i] + 48
+		z01.PrintRune(rune(rn))
+		counter++
+		if counter == 8 {
+			z01.PrintRune('\n')
+			counter = 0
 		}
 	}
-	return false
 }
 
-func printSolutions(allSolutions [][]cell) {
-	for _, sliceC := range allSolutions {
-		for _, cell := range sliceC {
-			z01.PrintRune(rune(cell.r + 48))
-		}
-		z01.PrintRune('\n')
-	}
-}
-
-// The length of a slice of cells
-func cSlLen(s []cell) int {
+func intSliceLen(s []int) int {
 	length := 0
+
 	for range s {
 		length++
 	}
+
 	return length
 }
