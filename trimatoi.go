3dc8b0c99bbf078aecc4c1ebc62c76877df1e3a6
Markus Amberla <markus.amberla@gmail.com>
2024-05-14 11:14:49 +0300

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -2,9 +2,15 @@ package piscine
 
 func TrimAtoi(s string) int {
 	numRunes := []rune{}
+	numsFound := false
 
 	for _, v := range s {
-		if (v >= '0' && v <= '9') || v == '-' {
+		if v >= '0' && v <= '9' {
+			numRunes = append(numRunes, v)
+			numsFound = true
+		}
+
+		if v == '-' && !numsFound {
 			numRunes = append(numRunes, v)
 		}
 	}
