cdbdafd4e174c15453d06dff8c196ed982def820
Markus Amberla <markus.amberla@gmail.com>
2024-05-15 09:05:40 +0300

    Arg nums to abcs

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"piscine"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	capitals := 0
+	skip1 := 0
+
+	if os.Args[1] == "--upper" {
+		capitals = -32
+		skip1 = 1
+	}
+
+	for i, v := range os.Args {
+		if i > 0+skip1 {
+			num := piscine.Atoi(v) - 1
+			myRune := 'a' + rune(num)
+			if myRune < 'a' || myRune > 'z' {
+				z01.PrintRune(' ')
+			} else {
+				runeToPrint := myRune + rune(capitals)
+				z01.PrintRune(runeToPrint)
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
