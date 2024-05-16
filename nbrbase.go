35cd178d8cdf81092a024f096289d00eb1e7d644
Markus Amberla <markus.amberla@gmail.com>
2024-05-16 21:35:47 +0300

    nbrbase

--- nbrbase.go
+++ nbrbase.go
@@ -157,7 +157,7 @@ func hiPowerMin(base int) int {
 	case 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77:
 		pows = 10
 	default:
-		pows = 9 // Correct up to base 128
+		pows = 6
 	}
 
 	return pows
