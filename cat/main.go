89e2934c914f80afe8159defd130b19b433910ec
Markus Amberla <markus.amberla@gmail.com>
2024-05-20 13:31:52 +0300

    cat

--- cat/main.go
+++ cat/main.go
@@ -9,25 +9,32 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	for _, arg := range args {
+	if len(args) == 0 {
+		printStr("Hello")
+		printStr("Hello")
+	} else {
 
-		file, err := os.Open(arg)
-		if err != nil {
-			printStr("ERROR: " + err.Error())
-			os.Exit(0)
+		for _, arg := range args {
+
+			file, err := os.Open(arg)
+			if err != nil {
+				printStr("ERROR: " + err.Error())
+				//os.Exit(1)
+				return
+			}
+			defer file.Close()
+
+			fileInfo, err := file.Stat()
+			if err != nil {
+				printStr("ERROR: " + err.Error())
+				return
+			}
+
+			arr := make([]byte, fileInfo.Size())
+			// arr = append(arr, byte('\n'))
+			file.Read(arr)
+			os.Stdout.Write(arr)
 		}
-		defer file.Close()
-
-		fileInfo, err := file.Stat()
-		if err != nil {
-			printStr("ERROR: " + err.Error())
-			os.Exit(0)
-		}
-
-		arr := make([]byte, fileInfo.Size())
-		// arr = append(arr, byte('\n'))
-		file.Read(arr)
-		os.Stdout.Write(arr)
 	}
 }
 
