eb7ac0282e9c266774a6d4b326aca0ff43519861
Markus Amberla <markus.amberla@gmail.com>
2024-05-21 08:28:47 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -1,13 +1,25 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	notSorted := false
+	orderNow := 0
+	firstOrder := 0
 	for i := 0; i < len(a)-1; i++ {
 		if f(a[i], a[i+1]) > 0 {
-			notSorted = true
+			orderNow = 1
+		}
+		if f(a[i], a[i+1]) < 0 {
+			orderNow = -1
+		}
+		if orderNow == 1 || orderNow == -1 && firstOrder == 0 {
+			firstOrder = orderNow
+		}
+		if orderNow == 1 || orderNow == -1 {
+			if orderNow != firstOrder {
+				return false
+			}
 		}
 	}
-	return !notSorted
+	return true
 }
 
 func f(a, b int) int {
