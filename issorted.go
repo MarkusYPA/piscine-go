1a862596a35706569b9f812760031a37b2ec805c
Markus Amberla <markus.amberla@gmail.com>
2024-05-21 08:18:44 +0300

    issorted

---
+++ issorted.go
@@ -0,0 +1,21 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	notSorted := false
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) > 0 {
+			notSorted = true
+		}
+	}
+	return !notSorted
+}
+
+func f(a, b int) int {
+	if a > b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
+}
