f6f0fdff05646894926462a7ce191502e10766ee
Markus Amberla <markus.amberla@gmail.com>
2024-05-21 15:34:20 +0300

    q1 revisited

--- teacher.sh
+++ teacher.sh
@@ -5,7 +5,7 @@ export IV_NUM=$699607
 echo ${IV_NUM}
 
 # Print out the interview
-cat mystery/interviews/interview-$699607
+cat mystery/interviews/interview-699607
 
 #export MAIN_SUSPECT=$main_suspect
 echo ${MAIN_SUSPECT}
