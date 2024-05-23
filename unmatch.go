582de3360a9427139e1258a57183b38ab1b50d60
Markus Amberla <markus.amberla@gmail.com>
2024-05-23 08:51:54 +0300

    unmatch.go

---
+++ unmatch.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Unmatch(a []int) int {
+	for _, v1 := range a {
+		mathces := 0
+		for _, v2 := range a {
+			if v1 == v2 {
+				mathces++
+			}
+		}
+		if mathces == 1 {
+			return v1
+		}
+	}
+	return -1
+}
