e0f450702e65c6b93bb5d133a67995ecc6209fcb
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 19:43:46 +0300

    List sort insert

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,38 +1,30 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-	if l == nil {
-		nd := &NodeI{Data: data_ref}
-		return nd
-	}
-
-	current := l
-	following := l.Next
-
-	if following == nil {
-		l.Next.Data = data_ref
+	if l == nil || l.Next == nil {
 		return l
 	}
 
-	if data_ref <= l.Data {
-		newNd := &NodeI{Data: data_ref, Next: l}
-		return newNd
-	}
+	current := l
+	subsequent := l.Next
 
-	for following != nil {
-		if current.Data <= data_ref && following.Data >= data_ref {
-			newNd := &NodeI{Data: data_ref, Next: following}
-			current.Next = newNd
-			current = newNd
+	for subsequent != nil {
+
+		// This can only happen for the first item on the list
+		if data_ref < current.Data {
+			newNode := &NodeI{Data: data_ref, Next: current}
+			return newNode
+		}
+
+		if data_ref < subsequent.Data {
+			newNode := &NodeI{Data: data_ref, Next: subsequent}
+			current.Next = newNode
 			return l
 		}
-		current = current.Next
-		following = current.Next
+
+		current = subsequent
+		subsequent = current.Next
 	}
 
-	if data_ref <= current.Data { // Current is now the last on the list
-		newNd := &NodeI{Data: data_ref}
-		current.Next = newNd
-	}
 	return l
 }
