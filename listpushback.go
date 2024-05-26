ab933123398e80f4dc83eea73875965103aac11f
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 16:23:36 +0300

    list push back

--- listpushback.go
+++ listpushback.go
@@ -10,17 +10,16 @@ type List struct {
 	Tail *NodeL
 }
 
-func insertNLback(tail *NodeL, data interface{}) *NodeL {
-	nL := &NodeL{Data: data}
-
-	if tail == nil {
-		return nL
-	} else {
-		tail.Next = nL
-		return nL
-	}
-}
-
 func ListPushBack(l *List, data interface{}) {
-	insertNLback(l.Tail, data)
+	newNode := &NodeL{Data: data, Next: nil} // "&"" because Head, Tail and Next use pointers
+
+	if l.Head == nil {
+		// If the list is empty, set both Head and Tail to the new node
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		// Otherwise, append the new node to the end of the list
+		l.Tail.Next = newNode
+		l.Tail = newNode
+	}
 }
