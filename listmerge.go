3693ebe484bf960380c1c50d7565aa538d1d348c
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 19:01:36 +0300

    List merge

--- listmerge.go
+++ listmerge.go
@@ -5,10 +5,12 @@ func ListMerge(l1 *List, l2 *List) {
 		if l2.Head != nil {
 			l1.Tail.Next = l2.Head
 		}
+		return
 	}
 
 	if l1 == nil && l2 != nil {
 		l1 = &List{l2.Head, l2.Tail}
+		return
 	}
 
 	if l1 != nil && l2 == nil {
