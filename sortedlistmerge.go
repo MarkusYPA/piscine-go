7b10cb190ae1231bf05106467e90e7eb29111a1e
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 19:52:53 +0300

    Sorted list merge

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -1,6 +1,19 @@
 package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
+	if n1 == nil && n2 != nil {
+		ListSort(n2)
+		return n2
+	}
+	if n1 != nil && n2 == nil {
+		ListSort(n1)
+		return n1
+	}
+
+	if n1 == nil && n2 == nil {
+		return n1
+	}
+
 	iterator := n1
 	for iterator.Next != nil {
 		iterator = iterator.Next
