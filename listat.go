0655bf02a33ab0673ce473e1c0bcf860cd259d15
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 16:59:07 +0300

    list for each

--- listat.go
+++ listat.go
@@ -1,5 +1,6 @@
 package piscine
 
+// Returns the node at index pos after l
 func ListAt(l *NodeL, pos int) *NodeL {
 	count := 0
 	current := l // Head
---
+++ listforeach.go
@@ -0,0 +1,26 @@
+package piscine
+
+func ListForEach(l *List, f func(*NodeL)) {
+	length := ListSize(l)
+	for i := 0; i < length; i++ {
+		f(ListAt(l.Head, i))
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
