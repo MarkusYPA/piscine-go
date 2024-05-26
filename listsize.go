2be53819ed8ccd7ff427028e15ff95beb9122924
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 16:31:58 +0300

    list size

--- listsize.go
+++ listsize.go
@@ -1,29 +1,5 @@
 package piscine
 
-func ListPushFront(l *List, data interface{}) {
-	newNode := &NodeL{Data: data, Next: nil}
-
-	if l.Head == nil {
-		// If the list is empty, set both Head and Tail to the new node
-		l.Head = newNode
-		l.Tail = newNode
-	} else {
-		// Otherwise, append the new node to the start of the list
-		newNode.Next = l.Head
-		l.Head = newNode
-	}
-}
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListSize(l *List) int {
 	count := 0
 	current := l.Head
