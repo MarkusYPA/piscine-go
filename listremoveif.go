7de44756209bedc7028e0e70b09cc331fe6a4f5b
Markus Amberla <markus.amberla@gmail.com>
2024-05-26 17:54:15 +0300

    List remove if

--- listremoveif.go
+++ listremoveif.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListRemoveIf(l *List, data_ref interface{}) {
 	if l == nil {
 		return
