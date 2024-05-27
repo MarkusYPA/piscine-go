494587ef4b1ad4954aef3d788f856b1074585438
Markus Amberla <markus.amberla@gmail.com>
2024-05-27 10:58:03 +0300

    apply by level

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,18 +1,53 @@
 package piscine
 
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
-	levels := BTreeLevelCount(root)
-	applyByLevel(root, f, levels)
-}
-
-func applyByLevel(node *TreeNode, f func(...interface{}) (int, error), level int) {
-	if node == nil {
+	if root == nil {
 		return
 	}
-	if level == BTreeLevelCount(node) {
-		f(node.Data)
+
+	// Initialize a queue
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+
+		// Traverse nodes at the current level
+		for i := 0; i < len(queue); i++ {
+
+			// Remove the first node from the queue and put it into a variable
+			node := queue[0]
+			queue = queue[1:]
+
+			// Apply function to current node
+			f(node)
+
+			// Add the left child to the queue
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			// Add the right child to the queue
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
 	}
-	level--
-	applyByLevel(node.Left, f, level)
-	applyByLevel(node.Right, f, level)
 }
+
+/*
+
+BTreeApplyByLevel(
+04
+├── 07
+│   ├── 12
+│   │   └── 10
+│   └── 05
+└── 01
+    └── 02
+        └── 03
+)
+ prints
+       + + +  +
+ "04010203071210" instead of
+ "0401070205120310"
+
+*/
