b3b3a69dd669df63d516a70490661182156bc5aa
Markus Amberla <markus.amberla@gmail.com>
2024-05-27 12:24:05 +0300

    Btree transplant

--- btreetransplant.go
+++ btreetransplant.go
@@ -4,8 +4,8 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 	toReplace := BTreeSearchItem(root, node.Data)
 	toReplace.Left = rplc.Left
 	toReplace.Right = rplc.Right
-	toReplace.Parent = rplc.Parent
 	toReplace.Data = rplc.Data
+	// Don't change the parent
 
 	return root
 }
