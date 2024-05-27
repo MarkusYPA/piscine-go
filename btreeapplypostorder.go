cf1b6359751e4e37bf5ee06b69e21ff49e5e1f3a
Markus Amberla <markus.amberla@gmail.com>
2024-05-27 08:48:17 +0300

    post order

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -8,3 +8,9 @@ func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	BTreeApplyInorder(root.Right, f)
 	f(root.Data)
 }
+
+/*
+prints
+"01\na\nb\nc\nd\nq\nr\nt\nx\ny\nz\n08\n" instead of
+"01\na\nd\nc\nb\nq\nr\nt\ny\nz\nx\n08\n"
+*/
