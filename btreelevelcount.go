f7972f8158bbd98321f02f04f65917f15d933b3c
Markus Amberla <markus.amberla@gmail.com>
2024-05-27 09:35:31 +0300

    level count

---
+++ btreelevelcount.go
@@ -0,0 +1,22 @@
+package piscine
+
+func BTreeLevelCount(root *TreeNode) int {
+	return levelCounter(root, 0)
+}
+
+func levelCounter(node *TreeNode, count int) int {
+	if node == nil {
+		return count
+	}
+
+	count++
+
+	left := levelCounter(node.Left, count)
+	right := levelCounter(node.Right, count)
+
+	if left > right {
+		return left
+	}
+
+	return right
+}
