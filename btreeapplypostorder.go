9ab2f4cf1283780023e356fe130dec55213366b4
Markus Amberla <markus.amberla@gmail.com>
2024-05-27 08:43:28 +0300

    post order

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -4,7 +4,7 @@ func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
 	BTreeApplyInorder(root.Left, f)
 	BTreeApplyInorder(root.Right, f)
+	f(root.Data)
 }
